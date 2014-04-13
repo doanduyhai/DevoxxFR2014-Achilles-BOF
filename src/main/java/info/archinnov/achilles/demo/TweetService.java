4c27a93c0e24db31d12bf88f55be02d9db69c385
DuyHai DOAN <doanduyhai@gmail.com>
2014-04-13 18:42:23 +0200

    Make tests 1 & 2 pass

--- src/main/java/info/archinnov/achilles/demo/TweetService.java
+++ src/main/java/info/archinnov/achilles/demo/TweetService.java
@@ -1,8 +1,11 @@
 package info.archinnov.achilles.demo;
 
+import static info.archinnov.achilles.demo.entity.TimeLineEntity.CompoundKey;
 import java.util.List;
 import java.util.UUID;
 import javax.inject.Inject;
+import org.apache.cassandra.utils.UUIDGen;
+import info.archinnov.achilles.demo.entity.TimeLineEntity;
 import info.archinnov.achilles.demo.model.TweetModel;
 import info.archinnov.achilles.persistence.PersistenceManager;
 
@@ -13,19 +16,23 @@ public class TweetService {
 
 
     public UUID createTweet(Long userId, String author, String content) {
-        //TODO create tweet given the author and content for the user
-        //TODO generate the tweet ID base on time and returns it
-        return null;
+        final UUID tweetId = UUIDGen.getTimeUUID();
+        manager.persist(new TimeLineEntity(userId, tweetId, author, content));
+        return tweetId;
     }
 
     public TweetModel findTweet(Long userId, UUID tweetId) {
-        //TODO find tweet by id for a given user
-        return null;
+        final TimeLineEntity entity = manager.find(TimeLineEntity.class, new CompoundKey(userId, tweetId));
+        return fromEntity(entity);
     }
 
     public List<TweetModel> getTweets(Long userId, UUID startTweetId, int fetchSize) {
         //TODO return last 'fetchSize' tweets, starting from 'startTweetId' excluded,
         //TODO for a given user
         return null;
+    }
+
+    private TweetModel fromEntity(TimeLineEntity entity) {
+        return new TweetModel(entity.getAuthor(), entity.getCompoundKey().getTweetId(), entity.getContent());
     }
 }
